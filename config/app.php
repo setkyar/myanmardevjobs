4d7e9c15ef16b5821f2d3564a567f805de533528
Set Kyar Wa Lar <setkyar16@gmail.com>
2015-09-08 15:05:35 +0630

    Add HTML and Sentinel Package

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Cartalyst\Sentinel\Laravel\SentinelServiceProvider',
 
     ],
 
@@ -193,6 +194,10 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Activation' => 'Cartalyst\Sentinel\Laravel\Facades\Activation',
+        'Reminder'   => 'Cartalyst\Sentinel\Laravel\Facades\Reminder',
+        'Sentinel'   => 'Cartalyst\Sentinel\Laravel\Facades\Sentinel',
 
     ],
 
